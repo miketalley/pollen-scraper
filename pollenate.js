8730a37170934ec9bdad24385fb33c99e415f308
Mike Talley <miketalley@hotmail.com>
2015-11-06 15:52:02 -0500

    Fixed issue with url being undefined

--- pollenate.js
+++ pollenate.js
@@ -18,7 +18,7 @@ var fs = require('fs'),
   cheerio = require('cheerio'),
   request = require('request'),
   url = require('url'),
-  Promise = require('promise').Promise,
+  Promise = require('promise'),
   _ = require('lodash'),
   bodyParser = require('body-parser');
 
@@ -54,6 +54,7 @@ function scrapeSite(req, res){
   var site = req.body.site,
     scraper = new Scraper(site);
 
+
   scraper.scrape([site]);
 
 }
@@ -73,7 +74,10 @@ function Scraper(siteUrl){
     var promises = [];
 
     urls.forEach(function(url){
-      promises.push(self.getLinksFromUrl(url));
+      var newLinksPromise = self.getLinksFromUrl(url);
+
+      console.log('Got new links promise', newLinksPromise);
+      promises.push(newLinksPromise);
     });
 
     Promise.all(promises).done(function(getLinksObjArray){
@@ -101,6 +105,7 @@ function Scraper(siteUrl){
       // Get HTML -- Needs promise
       self.getHtml(url).done(function(html){
         // Resolve with url and unique links
+        console.log("Html returned to getLinksFromUrl", url);
         resolve({
           url: url,
           links: self.getLinksFromHtml(html)
@@ -118,6 +123,7 @@ function Scraper(siteUrl){
       console.log('Requesting Site: ', site);
       request(site, function(error, response, html){
         if(!error){
+          console.log('Got HTML!');
           resolve(html);
         }
         else{
@@ -129,6 +135,7 @@ function Scraper(siteUrl){
   };
 
   this.fakeDOM = function(html){
+    console.log('Generating fake DOM');
     return cheerio.load(html);
   };
 
@@ -143,11 +150,12 @@ function Scraper(siteUrl){
 
     linksArray = _.uniq(linksArray);
 
+    console.log('Links found in HTML', linksArray.length);
     return linksArray;
     
     function fixLink(linkUrl){
-      var urlObj = url.parse(linkUrl),
-        thisHost = url.parse(this.site).host;
+      var urlObj = url.parse(linkUrl);
+        thisHost = url.parse(self.site).host;
 
       if(typeof linkUrl !== "string" || !linkUrl.length){ throw new Error("fixLink must be passed a valid string!");}
 
@@ -176,199 +184,6 @@ function Scraper(siteUrl){
       }
     });
   }
-
-
-  /*======================*/
-  /*======================*/
-  /*========OLD===========*/
-  /*=======BELOW==========*/
-  /*======================*/
-  /*======================*/
-
-
-  this.showScrapeResults = function(results, res){
-    results.forEach(function(result){
-      console.log(9876543210, result);
-    });
-
-    // res.send(results);
-  };
-  
-  this.getUniqueSiteLinks = function(url){
-    var checkedLinks = [],
-      uncheckedLinks = [],
-      pageLinks = this.getLinksFromPage(url);
-
-    console.log("Running getUniqueSiteLinks");
-
-    console.log("pageLinks Promise: ", pageLinks);
-    
-
-    return new Promise(function(resolve, reject){
-      pageLinks.done(function(links){
-        uncheckedLinks = uncheckedLinks.concat(links);
-        
-        self.processLinks(uncheckedLinks, checkedLinks)
-        .done(function(links){
-          console.log("Did I ever make it here?");
-          resolve(links);
-        });
-      });
-    });
-  };
-
-  // Checks if links are in list and returns them if they 
-  // are not, optional push flag will concat them to the list
-  this.checkNonDuplicateLinksInList = function(links, list, push){
-    var nonDuplicateLinks = links.filter(function(link){
-      return list.indexOf(link) === -1;
-    });
-
-    if(push){
-      nonDuplicateLinks.forEach(function(link){
-        list.push(link);
-      });
-    }
-
-    return nonDuplicateLinks;
-  };
-
-  this.processLinks = function(uncheckedLinksArray, checkedLinksArray){
-    if(!uncheckedLinksArray || !checkedLinksArray){
-      throw new Error("Bad parameters passed to processLinks!");
-    }
-
-    console.log(uncheckedLinksArray.length, checkedLinksArray.length);
-
-    return new Promise(function(resolve, reject){
-      while(uncheckedLinksArray.length){
-        var currentIndex = 0,
-          currentLink = uncheckedLinksArray[currentIndex],
-          uncheckedLinks, nonDuplicateUncheckedLinks;
-
-        if(currentLink){
-          uncheckedLinks = self.getLinksFromPage(currentLink);
-          uncheckedLinks.done(function(links){
-            console.log(8787, links.length);
-            nonDuplicateUncheckedLinks = self.checkNonDuplicateLinksInList(links, checkedLinksArray);
-
-            console.log(8686, nonDuplicateUncheckedLinks.length, uncheckedLinksArray.length);
-            self.checkNonDuplicateLinksInList(nonDuplicateUncheckedLinks, uncheckedLinksArray, true);
-            console.log(8585, nonDuplicateUncheckedLinks.length, uncheckedLinksArray.length);
-          });
-
-          checkedLinksArray.push(currentLink);
-        }
-        else{
-          console.log("Invalid link, disposing");
-        }
-        
-        uncheckedLinksArray.splice(currentIndex, 1);
-        console.log(uncheckedLinksArray.length, checkedLinksArray.length);
-      }
-
-      resolve(checkedLinksArray);
-    });
-  };
-
-  this.checkIfLinkIsAlreadyKnown = function(links){
-    var nonDuplicateUncheckedLinks = self.checkNonDuplicateLinksInList(links, checkedLinksArray);
-
-    console.log(111, uncheckedLinksArray.length);
-    self.checkNonDuplicateLinksInList(nonDuplicateUncheckedLinks, uncheckedLinksArray, true);
-    console.log(222, uncheckedLinksArray.length);
-  };
-
-}
-
-function Website(siteUrl){
-
-  this.site = siteUrl;
-
-  this.getHTML = function(site){
-    if(typeof site !== "string"){
-      throw new Error("No site passed to getHTML!");
-    }
-    
-    return new Promise(function(resolve, reject){
-      console.log('Requesting Site: ', site);
-      request(site, function(error, response, html){
-        if(!error){
-          resolve(html);
-        }
-        else{
-          console.log("Error!", error);
-          reject(error);
-        }
-      });
-      
-    });
-  };
-
-  this.fakeDOM = function(html){
-    return cheerio.load(html);
-  };
-
-  this.getBodyHTML = function($){
-    var $body = $('body');
-    
-    return $body.html();
-  };
-
-  // Returns a promise that is resolved with the links
-  // found within the HTML at the passed URL
-  this.getLinksFromPage = function(url){
-    var pageHTML = this.getHTML(url);
-
-    return new Promise(function(resolve, reject){
-      pageHTML.done(function(resp){
-        if(resp){
-          var links = self.getLinks(resp);
-
-          resolve(links);
-        }
-        else{
-          reject("No response");
-        }
-      });
-    });
-  };
-
-  this.getLinks = function(html){
-    var $ = this.fakeDOM(html),
-      links = $('a'),
-      linksArray = Array.prototype.slice.call(links);
-
-    linksArray = linksArray.map(function(link){
-      return self.fixLink(link.attribs.href);
-    });
-
-    linksArray = _.uniq(linksArray);
-
-    return linksArray;
-  };
-
-  // This method takes a linkUrl and checks it to
-  // see if it is a local/external link and also
-  // catches relative links and attaches the site
-  // url to them to try
-  this.fixLink = function(linkUrl){
-    var urlObj = url.parse(linkUrl),
-      thisHost = url.parse(this.site).host;
-
-    if(typeof linkUrl !== "string" || !linkUrl.length){ throw new Error("fixLink must be passed a valid string!");}
-
-    if(!urlObj.host){
-      return this.site + (linkUrl[0] === '/' ? linkUrl : '/' + linkUrl);
-    }
-    else if(urlObj.host === thisHost){
-      return linkUrl;
-    }
-    else{
-      return false;
-    }
-  };
-  
 }
 
 scrapeSite({
